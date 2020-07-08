190ae81da7d6386a05b3a59447faf198e9adf2b8
abdiasviana <abdviana@gmail.com>
2020-07-08 19:38:43 -0300

    creating the functions of trading, where you only can trade an item if the price that what youre sending is upper than what youre receiving

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -3,15 +3,45 @@ module Api
         class TradesController < ApplicationController
 
             def create
-                itemAmount = Inventory.all.where("survivor_id = ? and item_id = ?", params[:sender], params[:item_sender]).sum(:quantity)
+                itemAmount = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).sum(:quantity)
+                totalPriceSender = (Item.select(:value).where("id = ?", params[:item_id_sender]).sum(:value)) * params[:quantity_sender]
+                totalPriceReceiver = (Item.select(:value).where("id = ?", params[:item_id_receiver]).sum(:value)) * params[:quantity_receiver]
+                
+                survivorState = Survivor.all.where()
 
-                if itemAmount >= params[:quantity_sender]
-                    #valor do inventário é maior
+                puts "---------------"
+                puts itemAmount
+                puts totalPriceSender
+                puts totalPriceReceiver
+                puts "---------------"
+
+                if Survivor.exists?(id: params[:sender], infected: false) && Survivor.exists?(id: params[:receiver], infected: false)
+                    if itemAmount >= params[:quantity_sender]
+                        #valor do inventário é maior
+                        if totalPriceSender >= totalPriceReceiver
+                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:survivor_id_receiver], params[:item_id_sender]).empty?
+                                amountSending = itemAmount + params[:quantity_sender]
+                                Inventory.where("id = ?", params[:sender]).update(quantity: amountSending)
+                            else
+                                Inventory.create(quantity: params[:quantity_sender], item_id: params[:item_id_sender], survivor_id: params[:sender])
+                            end
+                        else
+                            render json: {
+                                status: 'ERROR',
+                                message: 'Deny. What youre sending to another survivor theres the price below what youre receiving'
+                            }, status: :unprocessable_entity
+                        end
+                    else
+                        # valor do inventário menor
+                        render json: {
+                            status: 'ERROR',
+                            message: 'No enough item on your inventory'
+                        }, status: :unprocessable_entity
+                    end
                 else
-                    # valor do inventário menor
                     render json: {
                         status: 'ERROR',
-                        message: 'No enough item on your inventory'
+                        message: 'Deny. Survivor infected'
                     }, status: :unprocessable_entity
                 end
             end
