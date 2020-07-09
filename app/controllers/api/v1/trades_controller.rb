1495a0ae3d1867a62647128bb4e277091ccf5fd7
abdiasviana <abdviana@gmail.com>
2020-07-09 12:40:37 -0300

    finishing the trading function

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -3,28 +3,57 @@ module Api
         class TradesController < ApplicationController
 
             def create
-                itemAmount = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).sum(:quantity)
+                subItemAmountSender = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).sum(:quantity)
+                subItemAmountReceiver = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_receiver]).sum(:quantity)
+                incItemAmountSender = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_receiver]).sum(:quantity)
+                incItemAmountReceiver = Inventory.select(:quantity).where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_sender]).sum(:quantity)
                 totalPriceSender = (Item.select(:value).where("id = ?", params[:item_id_sender]).sum(:value)) * params[:quantity_sender]
                 totalPriceReceiver = (Item.select(:value).where("id = ?", params[:item_id_receiver]).sum(:value)) * params[:quantity_receiver]
                 
                 survivorState = Survivor.all.where()
 
-                puts "---------------"
-                puts itemAmount
-                puts totalPriceSender
-                puts totalPriceReceiver
-                puts "---------------"
+                
 
                 if Survivor.exists?(id: params[:sender], infected: false) && Survivor.exists?(id: params[:receiver], infected: false)
-                    if itemAmount >= params[:quantity_sender]
-                        #valor do inventário é maior
+                    if subItemAmountSender >= params[:quantity_sender]
                         if totalPriceSender >= totalPriceReceiver
-                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:survivor_id_receiver], params[:item_id_sender]).empty?
-                                amountSending = itemAmount + params[:quantity_sender]
-                                Inventory.where("id = ?", params[:sender]).update(quantity: amountSending)
+                            
+                            # Decrement all the items that sender and receiver are trading
+                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).exists?
+                                subAmountSending = Integer(subItemAmountSender) - Integer(params[:quantity_sender])
+                                Inventory.where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).update(quantity: subAmountSending)
                             else
-                                Inventory.create(quantity: params[:quantity_sender], item_id: params[:item_id_sender], survivor_id: params[:sender])
+                                render json: {
+                                    status: 'ERROR',
+                                    message: 'No enough item that youre trying to trade on your inventory'
+                                }, status: :unprocessable_entity
                             end
+                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_receiver]).exists?
+                                subAmountReceiving = Integer(subItemAmountReceiver) - Integer(params[:quantity_receiver])
+                                Inventory.where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_receiver]).update(quantity: subAmountReceiving)
+                            else
+                                render json: {
+                                    status: 'ERROR',
+                                    message: 'No enough item that youre trying to trade on your inventory'
+                                }, status: :unprocessable_entity
+                            end
+
+                            # Updating what items that the sender and receiver has, or creating a new inventory if they doesnt
+                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_sender]).exists?
+                                incAmountReceiving = Integer(incItemAmountReceiver) + Integer(params[:quantity_sender])
+
+                                Inventory.where("survivor_id = ? and item_id = ?", params[:receiver], params[:item_id_receiver]).update(quantity: incAmountReceiving)
+                            else
+                                Inventory.create(quantity: params[:quantity_sender], item_id: params[:item_id_sender], survivor_id: params[:receiver])
+                            end
+                            if Inventory.all.where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_receiver]).exists?
+                                incAmountSending = Integer(incItemAmountSender) + Integer(params[:quantity_receiver])
+                                
+                                Inventory.where("survivor_id = ? and item_id = ?", params[:sender], params[:item_id_sender]).update(quantity: incAmountSending)
+                            else
+                                Inventory.create(quantity: params[:quantity_receiver], item_id: params[:item_id_receiver], survivor_id: params[:sender])
+                            end
+
                         else
                             render json: {
                                 status: 'ERROR',
@@ -32,7 +61,6 @@ module Api
                             }, status: :unprocessable_entity
                         end
                     else
-                        # valor do inventário menor
                         render json: {
                             status: 'ERROR',
                             message: 'No enough item on your inventory'
